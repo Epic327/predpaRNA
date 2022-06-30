c9d50de608e8cc99c406c573ec040d55eface8ad
程威 <chengv327@163.com>
2022-06-30 16:06:33 +0800

    去除多余注释和无用代码

--- app.py
+++ app.py
@@ -4,50 +4,46 @@ import gensim
 from keras_preprocessing.sequence import pad_sequences
 import re
 
-# 加载模型和预训练模型
-model = tf.keras.models.load_model('./model/ab-k1-s1-m600-2.h5')
-w2v_model = gensim.models.Word2Vec.load('./model/w2c_eRNA_k1_s1_d40')
+# 加载模型和word2vec预训练模型
+model = tf.keras.models.load_model('./model/')
+w2v_model = gensim.models.Word2Vec.load('./model/')
 
 
-def convert_data_to_index(string_data, wv):  # 把每一个mer都加上一个索引
+def convert_data_to_index(string_data, wv):
     index_data = []
-    for word in string_data:  # 遍历列表中的每一个字符串   string_data = list2
-        if word in wv:  # 如果这个字符串在WV ：应该是64种不同的mer  wv = model1.wv
-            index_data.append(wv.vocab[word].index)  # 就把这个字符串的索引加入一个新的列表index_data[]
+    for word in string_data:
+        if word in wv:
+            index_data.append(wv.vocab[word].index)
     return index_data
 
 
-def convert_sequences_to_index(list_of_sequences, wv):  # 把序列转换成索引
+def convert_sequences_to_index(list_of_sequences, wv):
     ll = []  #
-    for i in range(len(list_of_sequences)):  # 遍历所有的序列
-        ll.append(convert_data_to_index(list_of_sequences[i], wv))  # 把每一个序列都附上一个索引加入一个新的列表ll
+    for i in range(len(list_of_sequences)):
+        ll.append(convert_data_to_index(list_of_sequences[i], wv))
     return ll
 
 
-def seq2ngram(seqs, k, s, wv):  # 如果num《200000 返回的是[[],[],[],[]......，[]]索引
+def seq2ngram(seqs, k, s, wv):
     f = open(seqs)
     lines = f.readlines()
     f.close()
-    list22 = []  # 返回每一条序列k-mer对应的索引，构成的列表，列表数为序列的总数
-    print('need to n-gram %d lines' % len(lines))  # lines一共多少行
+    list22 = []
+    print('need to n-gram %d lines' % len(lines))
 
     for num, line in enumerate(lines):
         if num < 200000:
             line = line[:-1]  # remove '\n' and lower ACGT
             l = len(line)  # length of line
             list2 = []  # 所有序列的k-mer构成的列表
-            for i in range(0, l, s):  # 每一行序列的长度 ，步长s=1 ，k=3
-                if i + k >= l + 1:  # k-mer 滑过整条序列
+            for i in range(0, l, s):
+                if i + k >= l + 1:
                     break
-                list2.append(line[i:i + k])  # 取出 k-mer个碱基，加入列表list2
+                list2.append(line[i:i + k])  #
 
             list22.append(convert_data_to_index(list2, wv))  # 把索引和list2中的k-mer对应起来加入一个新的列表list22
 
     return list22
-
-
-def pred(message):
-    return ""
 
 
 # 实例flask
@@ -74,15 +70,6 @@ def predict():
     return render_template('predict.html')
 
 
-@app.route('/result1', methods=['GET', 'POST'])
-def file_predict():
-    if request.method == 'POST':
-        file = request.files["file"]
-
-        dict = pred(file)
-    return render_template('result.html', seq_dict=dict)
-
-
 @app.route('/result', methods=['POST'])
 def predict_result():
     patten = re.compile(r"[a-zBD-FH-SU-Z0-9!@#$%^&*()+_={}\[\]|\\:\"<>?/’~·.,，。、；：‘”【】？《》\s*\u4e00-\u9fa5]")
@@ -101,9 +88,6 @@ def predict_result():
         seq_num = message.count('>')
         # 划分序列的 name 和seq
         data = message.strip().split('\n')
-
-        # 取到 所有’>‘的位置索引
-        # name_index = [i for i in range(len(data)) if data[i].startswith('>')]
 
         seq_dict = {}  # 序列大字典
         seq_data = []  # 纯序列数据
